4db2357a73059a39b333ba680984eb1ff12160d0
Malo <malo.serot@etudiant.univ-reims.fr>
2022-02-20 18:15:20 +0100

    Recherche d'un pokémon

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,14 +1,29 @@
+import { useState } from "react";
+
+/**
+ * Affiche le contenu de la page d'accueil
+ * 
+ * @returns Page d'accueil
+ */
 const Home = () => {
+
+    const handleSubmit = (ev) => {
+        ev.preventDefault();
+        if (ev.target[0].value === "") return;
+        window.location.href = '/' + ev.target[0].value;
+        console.log(ev.target[0].value);
+    };
+
     return (
         <>
             <h2>Rechercher un pokémon</h2>
-            <div class="inline">
-                <input type="text" id="search-field" placeholder="Recherche..." />
-                <button id="search-btn" onClick="{}">Rechercher</button>
-            </div>
+            <form className="inline" onSubmit={handleSubmit}>
+                <input type="text" id="search-field" name="search" placeholder="Recherche..." autoFocus />
+                <input type="submit" id="search-btn" value="Recherche..." />
+            </form>
             
         </>
-    )
+    );
     
 };
   
